6e80a7c942c0c4bbb863602e127223ff6d105eaa
Pramod-135 <pramodroyseelam@gmail.com>
2026-03-26 14:46:10 +0530

    Add files via upload

---
+++ Cal_Main.java
@@ -0,0 +1,11 @@
+public class Cal_Main{
+	public static void main (String[] args){
+
+	Calculator cal = new Calculator();
+
+	System.out.println(cal.add(2,3));
+	System.out.println(cal.add(2,3,4));
+	System.out.println(cal.public_add(1.2,2.3));
+
+	}
+}
---
+++ Calculator.java
@@ -0,0 +1,18 @@
+class Calculator{
+
+	public int add(int a, int b){
+		return a+b;
+	}
+
+	protected int add(int a, int b, int c){
+		return a+b+c;
+	}
+
+	private double add(double a, double b){
+		return a+b;
+	}
+
+	public double public_add(double a, double b){
+		return a+b;
+	}
+}
